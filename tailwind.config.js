1e460e408557646a3b2512425027683be102ef99
z m <328393419@qq.com>
2024-06-24 15:12:25 +0800

    初始化

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // 或者 'media' 或者 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
